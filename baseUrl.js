362977ece38b84020bbe3929c36b6495982ddb20
Thiago Rodrigues Teixeira <thiagotx2019@gmail.com>
2024-03-14 20:30:03 -0300

    permissão dada a restaurantes app

---
+++ baseUrl.js
@@ -0,0 +1,6 @@
+//export default 'http://localhost:3000';
+
+export default 'https://api-ecommerce-supermercado-familia.thiagosoftwareengineer.shop';
+
+
+
--- src/components/functions/baseUrl.js
+++ src/components/functions/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = 'https://restaurante-api-rouge.vercel.app';
+const baseUrl = 'https://restaurantes-api.thiagosoftwareengineer.shop';
 //const baseUrl = 'http://localhost:8080';
 export default baseUrl;
